24d940b86da38a93ec85f603d7e2086b0cebeb61
rezarmdani <153246198+rezarmdani@users.noreply.github.com>
2023-12-22 08:08:02 +0700

    pemweb

---
+++ index (1).php
@@ -0,0 +1,71 @@
+<?php include 'header.php';?>
+      
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+            <img class="rounded-circle" width="300" height="300" src="https://statik.tempo.co/data/2023/09/21/id_1238832/1238832_720.jpg" alt="">
+          <h1 class="display-4">JUJUTSU KAISEN</h1>
+          <p class="lead">KOTA SATRIA</p>
+        </div>
+      </div>
+
+  <div class="container text-center" id="About">
+    <div class="row">
+    <div class="col">
+            <h1>Tentang Banyumas</h1>
+  </div>
+  </div>
+    <div class="row">
+    <div class="col text-justify">
+        <p>Jujutsu Kaisen (Jepang: 呪術廻戦, terj. har. "Pertarungan Sihir") adalah sebuah seri manga shōnen asal Jepang yang ditulis dan diilustrasikan oleh Gege Akutami. Manga ini dimuat berseri dalam majalah Weekly Shōnen Jump terbitan Shueisha sejak Maret 2018, dan telah diterbitkan menjadi dua puluh empat volume tankōbon per Oktober 2023..</p>
+</div>
+<div class="col text-justify">
+        <p>Viz Media menerbitkan tiga bab pertamanya untuk program "Jump Start" mereka. Shueisha mulai merilis Jujutsu Kaisen dalam bahasa Inggris secara serentak dengan rilisnya di Jepang melalui platform Manga Plus sejak Januari 2019.</p>
+</div>
+</div>
+</div>
+<div class="container text-center">
+<div class="row">
+<div class="col">
+    <h1>Portofolio</h1>
+</div>
+</div>
+
+  <div class="card-deck mt-5 ml-5 mr-5">
+    <div class="card">
+      <img class="card-img-top" src="https://i.pinimg.com/736x/dc/8b/8b/dc8b8baea4703465d35585be0a48e72f.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Yuji Itadori</h5>
+        <p class="card-text"></p>
+        <p class="card-text"><small class="text-muted">Last updated 3 week ag</small></p>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top" src="https://tulisin.kekitaan.com/wp-content/uploads/2021/06/83053b171c235571b983c0a5e329c156-500x500.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Satoru Gojo</h5>
+        <p class="card-text">Wisata Baturraden</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 week ago</small></p>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top" src="https://i.pinimg.com/736x/59/2a/dd/592add56a05650a6a20ee9feed884504.jpg">
+      <div class="card-body">
+        <h5 class="card-title">Megumi Fushiguro</h5>
+        <p class="card-text"></p>
+        <p class="card-text"><small class="text-muted">Last updated 3 week ag</small></p>
+      </div>
+    </div>
+  </div>
+
+
+ 
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+<?php include 'footer.php';?>
